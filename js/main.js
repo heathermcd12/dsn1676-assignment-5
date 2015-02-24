7411cb8e34521e0a292ce3296e1883d7c2f4cf89
heathermcd12 <heather-mcdonell@hotmail.com>
2015-02-24 14:13:53 -0500

    cluster of deletions

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 var $form = $('form');
-var $toDoList = $('#to-do-list'); //# because its an id..?
-var $list = $('.list');
-var $removeBtn = $('');
+var $todo = $('.todo'); 
+var $added = $('.added');
+var $button = $('.addbutton');
+
 $form.on('submit', function (e) {
-//Stop the form from doing what it normally does
-  var $li = $('<li>');
-  e.preventDefault();
-  //$toDoList.val())
-  $li.append($toDoList.val()) //in order for the todolist to add stuff to the li, you must add stuff from the li to the list
-  $list.append($li) //value for this is not an input
-  
-  $li.on('click', function () {
 
-       $li.remove(); 
+    e.preventDefault();
+    var $li = $('<li>');
+    var $button = $('<button>');
+    $button.html('X');
+    $button.addClass('check');
+    $button.on('click', function () {
+        $li.remove(); 
+    });   
+    
+    var $todoH2 = $('<h2>').html($todo.val());
+    $todoH2.append($button);
+    
+    $todoH2.on('click', function () {
+    $todoH2.addClass ('strikethrough') ();
     });
+    
+    $li.append($todoH2);
+    $added.append($li);
   
 });
 
-$removeBtn.on('click', function () {
-  var offset = $circle.offset();
 
-  $circle.css('left', offset.left + 40);              
-});
+
 
 
